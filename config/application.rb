dd68434f351a85b503fef881facf1f310c3e23cc
sarii0213 <anjing.singka@gmail.com>
2022-07-25 08:54:55 +0900

    :sparkles: Add config not to generate helpers

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,10 @@ module InstaCloneVer7
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.helper false
+    end
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
   end
