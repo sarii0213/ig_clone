5cb52f7eba26d94df7bdc29d7f8ac935b621f0f8
sarii0213 <anjing.singka@gmail.com>
2022-08-10 09:47:30 +0900

    :sparkles: Add profile edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root to: 'posts#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
@@ -13,5 +14,9 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resource :relationship, only: %i[create destroy], module: :users
+  end
+
+  namespace :mypage do
+    resource :account, only: %i[edit update]
   end
 end
